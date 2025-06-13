12bdf7378e975af3cb5a550e0c2e89c631a11d01
dineshviriyala <149519385+dineshviriyala@users.noreply.github.com>
2025-06-13 19:50:59 +0530

    Delete backend directory

--- backend/models/Apartment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const apartmentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  apartmentCode: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model('Apartment', apartmentSchema);
